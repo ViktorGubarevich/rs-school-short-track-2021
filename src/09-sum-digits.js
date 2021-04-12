18264f6a301d3f1b7ea68b8fd83440aef0544db7
Viktor Gubarevich <gubarew.v@gmail.com>
2021-04-12 22:52:18 +0300

    feat: 09-sum

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,13 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let num = n.toString(10);
+  while (num.length > 1) {
+    const sum = num.split('').map((x) => +x).reduce((a, b) => a + b, 0);
+    num = sum.toString(10);
+  }
+  return Number(num);
 }
 
 module.exports = getSumOfDigits;
