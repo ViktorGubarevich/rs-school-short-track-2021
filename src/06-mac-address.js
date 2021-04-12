6ce9840b5d234e6f9d4b103ea2fda055ce31d8fe
Viktor Gubarevich <gubarew.v@gmail.com>
2021-04-12 22:43:10 +0300

    feat: 06-mac

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,17 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const arr = n.split('-');
+  if (arr.length !== 6) return false;
+  for (let i = 0; i < arr.length; i++) {
+    const e = arr[i];
+    if (Number.isNaN(parseInt(e, 16)) || parseInt(e, 16) > 0xff) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 module.exports = isMAC48Address;
