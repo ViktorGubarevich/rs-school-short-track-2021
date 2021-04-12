338296516bc1ea80a3427627ac568093f0c91fdd
Viktor Gubarevich <gubarew.v@gmail.com>
2021-04-12 23:09:45 +0300

    feat: 11-encode

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const arr = str.split('');
+  const result = [];
+  let e = 1;
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i - 1] !== arr[i]) {
+      result.push(arr[i]);
+      e = 1;
+    } else {
+      e += 1;
+      result[result.length - 1] = `${e}${arr[i]}`;
+    }
+  }
+  return result.join('');
 }
 
 module.exports = encodeLine;
